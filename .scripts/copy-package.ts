5b90a7f9f5289a7a1b1af60a937259d8532e4a21
Dan Erdelt <erd@conceptual.ca>
2022-08-14 05:34:16 +0000

    postinstall hook

--- .scripts/copy-package.ts
+++ .scripts/copy-package.ts
@@ -1,11 +1,13 @@
-import { PackageJson } from "type-fest";
 import { readJsonFile, writeJsonFile } from "@cpdevtools/lib-node-utilities";
+import { PackageJson } from "type-fest";
 
 (async () => {
   const pkg = (await readJsonFile("./package.json")) as PackageJson;
   delete pkg.devDependencies;
-  delete pkg.scripts;
   delete (pkg as any)["lint-staged"];
+  pkg.scripts = {
+    postinstall: "devhost postinstall",
+  };
 
   await writeJsonFile("./dist/main/package.json", pkg, 2);
 })();
