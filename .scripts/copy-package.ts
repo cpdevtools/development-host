9f02cf29aa0e8db0e679d548f635debd256fef7e
Dan Erdelt <erd@conceptual.ca>
2022-08-14 12:18:37 +0000

    script

--- .scripts/copy-package.ts
+++ .scripts/copy-package.ts
@@ -1,10 +1,13 @@
-import { PackageJson } from "type-fest";
 import { readJsonFile, writeJsonFile } from "@cpdevtools/lib-node-utilities";
+import { PackageJson } from "type-fest";
 
 (async () => {
   const pkg = (await readJsonFile("./package.json")) as PackageJson;
   delete pkg.devDependencies;
-  delete pkg.scripts;
+  //delete pkg.scripts;
+  pkg.scripts = {
+    postinstall: "devhost install",
+  };
   delete (pkg as any)["lint-staged"];
 
   await writeJsonFile("./dist/main/package.json", pkg, 2);
