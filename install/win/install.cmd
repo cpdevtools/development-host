3529c5e6cac321ce73db1fd4f6175e6717b5ee74
Dan Erdelt <erd@conceptual.ca>
2022-08-14 03:46:52 +0000

    install strategy change

--- install/win/install.cmd
+++ install/win/install.cmd
@@ -1,5 +1,4 @@
 @echo off
-
 setlocal
   echo [95m
   echo ************************************************************
@@ -9,41 +8,6 @@ setlocal
   echo ************************************************************
   echo [0m
 
-  set "repo=https://raw.githubusercontent.com/cpdevtools/development-host/main"
-  set "rng=%random%%random%%random%"
-  set "tmpPath=%temp%\cpdevtools\development-host\install\%rng%"
-  mkdir %tmpPath% > nul 2>&1
-
-  echo [96mDetecting Windows version...[0m
-
-  for /f "tokens=4-7 delims=. " %%i in ('ver') do (
-      set fullVersion=%%i.%%j.%%k.%%l
-      set mainVersion=%%i
-      set subVersion=%%k
-  )
-
-  if "%mainVersion%" == "10" if not "%subVersion%" == "22000" goto install_10
-  if "%mainVersion%" == "10" if "%subVersion%" == "22000" goto install_11
-
-  echo [91mUnsupported Windows Version %fullVersion%[0m
-  exit 1
-
-  :install_10
-    echo [96mDownloading  Windows [93m10[96m Installer...[0m
-    curl --ssl --silent  %repo%/install/win/win10.cmd > %tmpPath%\install.cmd
-    cd %tmpPath%
-    echo [92mStarting Windows [93m10[92m Installer.[0m
-    install.cmd
-  goto end
-
-  :install_11
-    echo [96mDownloading  Windows [93m11[96m Installer...[0m
-    curl --ssl --silent  %repo%/install/win/win11.cmd > %tmpPath%\install.cmd
-    cd %tmpPath%
-    echo [92mStarting Windows [93m11[92m Installer.[0m
-    install.cmd
-  goto end
-
-
-  :end
+  winget install -e --id OpenJS.NodeJS.LTS
+  npm i -g @cpdevools/development-host
 endlocal
