5488f088b377b72a019458e22fcd23f63165e97c
Dan Erdelt <erd@conceptual.ca>
2022-08-19 00:47:07 +0000

    node install fix

--- src/lib/_install_data_/installers/nvm.installer.ts
+++ src/lib/_install_data_/installers/nvm.installer.ts
@@ -2,7 +2,7 @@ import { BashInstaller, Installer } from "@cpdevtools/lib-node-utilities";
 
 const installOrUpdateScript = `
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-export NVM_DIR="/usr/local/share/nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm --version
